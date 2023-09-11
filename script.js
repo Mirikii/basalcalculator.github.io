a5354f301c1313c7ab600cba2268b889a4de9e8f
Alan <144269465+Mirikii@users.noreply.github.com>
2023-09-11 13:56:26 -0400

    1 Code
    Finalizing_details.

---
+++ script.js
@@ -0,0 +1,31 @@
+const ERROR = document.getElementById("error");
+const FLU = document.getElementById("flu");
+const MAN = document.getElementById("man");
+const INPUT = document.getElementById("peso");
+const BOTON = document.getElementById("calcular");
+
+BOTON.addEventListener("click", () => {
+let peso = INPUT.value;
+console.log(peso)
+if (peso > 0){
+    ERROR.style.display = "none";
+   let res = holliday(peso);
+    } else {
+        console.log("error")
+        ERROR.style.display = "block"
+    }
+});
+   
+function holliday(peso) {
+    console.log("peso")
+    let volumen;
+    if (peso <= 10){
+        volumen = peso*100
+    } else if (peso >10 && peso <= 20){
+        volumen = 1000 + (peso-10)*50;
+    } else {
+        volumen = 1500 + (peso-20)*20;
+    } 
+    return volumen
+}
+
